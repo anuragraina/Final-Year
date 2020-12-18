90db1152428c2777746973ac5c5dba79c48b30f2
Gaurav29Shinde <gaurav29shinde@gmail.com>
2020-12-18 21:10:37 +0530

    Add Coarse Aggregate and Water Absorption screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'screens/home_page/index.dart';
 import './screens/material _testing/material_testing.dart';
 import './screens/fine_aggregate/fine_aggregate.dart';
+import './screens/coarse_aggregate/coarse_aggregate.dart';
 import './screens/fine_aggregate/moisture_content.dart';
 import './screens/fine_aggregate/specific_gravity.dart';
 import './screens/fine_aggregate/silt_content.dart';
+import './screens/coarse_aggregate/water_absorption.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,9 +40,11 @@ class MyApp extends StatelessWidget {
         routes: {
           '/material-testing': (ctx) => MaterialTesting(),
           '/fine-aggregate': (ctx) => FineAggregate(),
+          '/coarse-aggregate': (ctx) => CoarseAggregate(),
           '/fine-aggregate/moisture-content': (ctx) => MoistureContent(),
           '/fine-aggregate/specific-gravity': (ctx) => SpecificGravity(),
           '/fine-aggregate/silt_content': (ctx) => SiltContent(),
+          '/coarse-aggregate/water-absorption': (ctx) => WaterAbsorption(),
         });
   }
 }
---
+++ lib/screens/coarse_aggregate/coarse_aggregate.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/test_card.dart';
+
+class CoarseAggregate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coarse Aggregate'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        children: [
+          TestCard(name: 'Petrographical analysis'),
+          TestCard(name: 'Grading Analysis'),
+          TestCard(
+            name: 'Moisture content ',
+          ),
+          TestCard(
+            name: 'Elongation Index and Flakiness Index',
+          ),
+          TestCard(name: 'Deleterious material'),
+          TestCard(name: 'Specific gravity'),
+          TestCard(
+            name: 'Water Absorption',
+            route: '/coarse-aggregate/water-absorption',
+          ),
+          TestCard(name: 'Organic impurity'),
+          TestCard(name: 'Chloride Content & Sulphate Content'),
+          TestCard(name: 'Alkali aggregate reactivity'),
+        ],
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+    );
+  }
+}
